03205e88ec1fbf03d3cf8a9de22dd1ba6c1bc0cc
rencire <546296+rencire@users.noreply.github.com>
2025-05-22 17:49:28 -0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+This is an example repo for installing nixos on my old 2010 macbook pro, semi-remotely.
+
+For more info, see blog post: <link to blog post>
--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "A basic flake";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.flakelight.url = "github:accelbread/flakelight";
 
   outputs =
